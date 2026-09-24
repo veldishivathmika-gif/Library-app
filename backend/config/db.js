ef9bc1d32cc6baa13b1b6c75b441a303917c0eb0
veldishivathmika-gif <veldishivathmika@gmail.com>
2026-09-24 18:50:59 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
   try {
     
-    await mongoose.connect("mongodb://localhost:27017/library");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected:', mongoose.connection.host);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
